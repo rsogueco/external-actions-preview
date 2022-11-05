1f055c081830deb0d3e7f0c7c204fe3ee9493d55
Ron Sogueco <ron@soguecode.io>
2022-11-05 13:10:20 -0400

    Add initial components

---
+++ ExternalActionsPreviewer/force-app/main/default/aura/ExternalActionPreviewerSelector/ExternalActionPreviewerSelectorController.js
@@ -0,0 +1,14 @@
+({
+    init : function (component, event, helper ) {
+        component.set("v.items", {
+            type: 'icon',
+            href: '',
+            label: 'Some External Action',
+            iconName: 'standard:maintenance_work_rule',
+            alternativeText: 'Some External Action',
+        });
+    },
+	handleKeyUp : function(component, event, helper) {
+		
+	}
+})
